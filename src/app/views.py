38edd9c6d79be9ea04a14860ddf92c939e20004c
erikkaBaguio <erikka.baguio@gmail.com>
2016-12-02 07:56:06 +0800

    [Add Restaurant] - Implement route in Flask to accomodate insert operation

--- src/app/views.py
+++ src/app/views.py
@@ -11,3 +11,24 @@ spcalls = SPcalls()
 
 
 @app.route('/api/foodcart/restaurants/', methods = ['POST'])
+def store_restaurant():
+	data = json.loads(request.data)
+
+	resto_name = data['resto_name']
+	min_order = data['min_order']
+	delivery_fee = data['delivery_fee']
+	location = data['location']
+
+	if ( resto_name == '' or not min_order or not delivery_fee ):
+	
+		return jsonify({"status": "FAILED", "message": "Please fill the required fields"})
+	
+	else:
+	
+		restaurant = spcalls.spcall('store_restaurant',(resto_name, min_order, delivery_fee, location),True)
+		
+		if 'Error' in str(restaurant[0][0]):
+			return jsonify({"status": "FAILED", "message": restaurant[0][0]})
+
+		else:
+			return jsonify({"status": "OK", "message": restaurant[0][0]})
