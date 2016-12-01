82e4fa511600e43a1bfa6930067a68f9cfd3d1a1
erikkaBaguio <erikka.baguio@gmail.com>
2016-12-02 07:37:52 +0800

    Setup table for Orders in tables.sql

--- src/db_scripts/tables.sql
+++ src/db_scripts/tables.sql
@@ -1,15 +1,34 @@
-CREATE TABLE Food
+create table Food
 (	id				SERIAL8 PRIMARY KEY,
-	food_name		TEXT NOT NULL,
+	food_name		VARCHAR(200) NOT NULL,
 	description		TEXT NOT NULL,
 	unit_cost		FLOAT,
 	is_active		BOOLEAN DEFAULT TRUE
 );
 
 
-CREATE TABLE Category
+create table Category
 (
 	id           	SERIAL8 PRIMARY KEY,
-	category_name	TEXT NOT NULL,
+	category_name	VARCHAR(100) NOT NULL,
 	is_active       BOOLEAN DEFAULT TRUE
+);
+
+create table Restaurant
+(
+	id           	SERIAL8 PRIMARY KEY,
+	resto_name 		VARCHAR(100) NOT NULL,
+	min_order		FLOAT,
+	delivery_fee	FLOAT,
+  	is_active       BOOLEAN DEFAULT TRUE
+);
+
+
+create table Orders
+(
+	id 					SERIAL8 PRIMARY KEY,
+	role_id				INT REFERENCES Roles(id),
+	payment_id			INT,
+	order_foods_id		INT REFERENCES Order_foods(id),
+	subtotal			FLOAT
 );
