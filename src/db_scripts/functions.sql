9919506ca2423aa4ab17721c144db49abc3a719d
erikkaBaguio <erikka.baguio@gmail.com>
2016-12-02 07:42:57 +0800

    [Add Restaurant] - Write insert function in stored procedure

---
+++ src/db_scripts/functions.sql
@@ -0,0 +1,30 @@
+--[POST] Add new restaurant details
+--select store_restaurant('Cozy Cup', 10, 0, 'San Miguel, Iligan City')
+create or replace function store_restaurant(par_restoName text, par_minOrder float, par_deliveryFee float, par_location text)
+	returns text as
+	$$
+		DECLARE
+			local_response text;
+		BEGIN
+
+			if  par_restoName = '' or
+				par_minOrder is NULL or
+				par_deliveryFee is NULL or
+				par_location = '' 
+			
+			THEN
+
+				local_response = 'ERROR';
+			ELSE	
+
+			insert into Restaurant(resto_name, min_order, delivery_fee, location)
+			values (par_restoName, par_minOrder, par_deliveryFee, par_location);
+			
+			local_response = 'OK';
+			
+			END IF;
+
+			return local_response;
+		END;
+	$$
+		language 'plpgsql';
