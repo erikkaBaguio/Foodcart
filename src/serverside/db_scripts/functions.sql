97e952e27865e70803744e7ef9c77ca37b37279b
Kristel Pabillaran <pabillarankristel@ymail.com>
2016-12-02 10:28:47 +0800

    [Sign up] - Write insert function in stored procedure

--- src/serverside/db_scripts/functions.sql
+++ src/serverside/db_scripts/functions.sql
@@ -28,3 +28,28 @@ create or replace function store_restaurant(par_restoName varchar, par_minOrder 
 		END;
 	$$
 		language 'plpgsql';
+
+
+create or replace function store_user(par_fname TEXT, par_mname TEXT, par_lname TEXT, par_address TEXT, par_email TEXT,
+									 par_mobileNum TEXT, par_password TEXT, par_roleID INT, par_points INT)
+	returns text as
+	$$
+		DECLARE
+			loc_res TEXT;
+		BEGIN
+			if par_fname = '' or par_mname = '' or par_lname = '' or par_address = '' or par_email = '' or par_mobileNum = NULL or par_password = ''
+							or par_roleID = NULL or par_points = NULL THEN
+				loc_res = 'Error';
+			ELSE
+				INSERT INTO Userinfo(fname, mname, lname, address, email, mobile_number, user_password, role_id, earned_points) VALUES (par_fname, par_mname,
+								par_lname, par_address, par_email, par_mobileNum, par_password, par_roleID, par_points);
+
+				loc_res = 'OK';
+			END IF;
+
+			RETURN loc_res;
+		END;
+	$$
+		language 'plpgsql';
+
+--select store_user('ahlaine', 'gem', 'pabs', 'iligan', 'gem@gmail.com', '0123', 'asas', 1, '0');
