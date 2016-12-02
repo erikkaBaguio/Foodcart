342bff54d8a3c9345f9d544f6b05e2b488a1c725
erikkaBaguio <erikka.baguio@gmail.com>
2016-12-02 23:30:29 +0800

    [Add Restaurant] - Link the route to the UI using AJAX

---
+++ src/clientside/main.js
@@ -0,0 +1,70 @@
+var auth_user = "";
+var user_role;
+var timer = 0; 
+
+$(document).ready(function(){
+
+
+});
+
+
+function storeRestaurant(){
+	var resto_name = $('#resto_name').val();
+	var min_order = $('#min_order').val();
+	var delivery_fee = $('#delivery_fee').val();
+	var location = $('#location').val();
+
+	var data = JSON.stringify({ 'resto_name' : resto_name,
+								'min_order' : min_order,
+								'delivery_fee' : delivery_fee,
+								'location' : location	
+							});
+
+	$.ajax({
+	    	type:"POST",
+	    	url: "http://localhost:5000/api/foodcart/restaurants/",
+	    	contentType:"application/json; charset=utf-8",
+			data:data,
+			dataType:"json",
+
+			success: function(results){
+				if (results.status == 'OK'){
+
+					$('#add-resto-alert').html(
+						'<div class="alert alert-success"><strong>Success ' +
+						 '!</strong>' + results.message +'</div>');
+
+					$("#welcome-alert-nurse").fadeTo(2000, 500).slideUp(500);
+
+					clearAssessmentForm();
+
+				}
+
+				if(results.status == 'FAILED'){
+
+					$('#add-resto-alert').html(
+						'<div class="alert alert-danger"><strong>Failed ' +
+						 '!</strong>' + results.message +'</div>');
+
+					$("#add-resto-alert").fadeTo(2000, 500).slideUp(500);
+
+				}
+			},
+			error: function(e){
+				alert("THIS IS NOT COOL. SOMETHING WENT WRONG: " + e);
+			},
+			beforeSend: function (xhrObj){
+
+	      		xhrObj.setRequestHeader("Authorization", "Basic " + btoa( auth_user ));
+
+	        }
+	    });
+}
+
+
+function clearRestaurantForm(){
+	var restaurant_form = document.getElementById("restaurant-form");
+	var restaurant_form_2 = document.getElementById("restaurant-form-2");
+	restaurant_form.reset();
+	restaurant_form_2.reset();
+}
--- src/serverside/app/views.py
+++ src/serverside/app/views.py
@@ -32,3 +32,16 @@ def store_restaurant():
 
 		else:
 			return jsonify({"status": "OK", "message": restaurant[0][0]})
+
+
+@app.after_request
+def add_cors(resp):
+    resp.headers['Access-Control-Allow-Origin'] = flask.request.headers.get('Origin', '*')
+    resp.headers['Access-Control-Allow-Credentials'] = True
+    resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET, PUT, DELETE'
+    resp.headers['Access-Control-Allow-Headers'] = flask.request.headers.get('Access-Control-Request-Headers',
+                                                                             'Authorization')
+    # set low for debugging
+    if app.debug:
+        resp.headers["Access-Control-Max-Age"] = '1'
+    return resp
