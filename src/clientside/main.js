48e7175044a3bd54adc923eb8000900e13058f0f
erikkaBaguio <erikka.baguio@gmail.com>
2016-12-04 00:37:50 +0800

    [Update Restaurant] - Link the route to the UI using AJAX

--- src/clientside/main.js
+++ src/clientside/main.js
@@ -92,6 +92,7 @@ function viewAllRestaurant(){
 										// '<td>' + results.entries[i].delivery_fee + '</td>' +
 										// '<td>' + results.entries[i].location + '</td>' +
 										'<td>'+'<button onclick="viewRestaurantById('+ results.entries[i].restaurant_id +'); $(\'#view-resto\').show();$(\'#view-all-resto\').hide()" class="btn btn-info">View</button>'+'</td>'+
+										'<td>'+'<button onclick="updateRestaurant('+ results.entries[i].restaurant_id +'); $(\'#update-resto-form\').show();$(\'#view-all-resto\').hide()" class="btn btn-danger">Update</button>'+'</td>'+
 									 '</tr>';
 
 						restaurant_row  += restaurant
@@ -183,4 +184,60 @@ function viewRestaurantById(restaurant_id){
 		}
 
 	});
+}
+
+
+function updateRestaurant(restaurant_id){
+	var resto_name = $('#update_resto_name').val();
+	var min_order = $('#update_min_order').val();
+	var delivery_fee = $('#update_delivery_fee').val();
+	var location = $('#update_location').val();
+	var restaurant_id = restaurant_id;
+
+	var data = JSON.stringify({ 'restaurant_id': restaurant_id,
+									'resto_name' : resto_name,
+									'min_order' : min_order,
+									'delivery_fee' : delivery_fee,
+									'location' : location	
+								});
+	
+	$.ajax({
+		type:"PUT",
+    	url: "http://localhost:5000/api/foodcart/restaurants/",
+    	contentType:"application/json; charset=utf-8",
+		data:data,
+		dataType:"json",
+
+		success: function(results){
+				if (results.status == 'OK'){
+
+					$('#update-alert').html(
+						'<div class="alert alert-success"><strong>Success ' +
+						 '!</strong>' + results.message +'</div>');
+
+					$("#update-alert").fadeTo(2000, 500).slideUp(500);
+
+					clearRestaurantForm();
+
+				}
+
+				if(results.status == 'FAILED'){
+
+					$('#update-alert').html(
+						'<div class="alert alert-danger"><strong>Failed ' +
+						 '!</strong>' + results.message +'</div>');
+
+					$("#update-alert").fadeTo(2000, 500).slideUp(500);
+
+				}
+			},
+			error: function(e){
+				alert("THIS IS NOT COOL. SOMETHING WENT WRONG: " + e);
+			},
+			beforeSend: function (xhrObj){
+
+	      		xhrObj.setRequestHeader("Authorization", "Basic " + btoa( auth_user ));
+
+	        }
+	    });
 }
