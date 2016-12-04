20e2d102319d6d098b933fe1116fc02725e4a74b
erikkaBaguio <erikka.baguio@gmail.com>
2016-12-04 21:52:14 +0800

    [Deactivate Restaurant] - Testing - Added steps in steps.py

--- src/serverside/test/bdd/steps/steps.py
+++ src/serverside/test/bdd/steps/steps.py
@@ -10,50 +10,6 @@ import json
 def before_all():
     world.app = app.test_client()
 
-@step(u'Given the system administrator have the following restaurant details:')
-def given_the_system_administrator_have_the_following_restaurant_details(step):
-    world.restaurant = step.hashes[0]
-
-@step(u'When  the system administrator clicks the send button')
-def when_the_system_administrator_clicks_the_send_button(step):
-    world.browser = TestApp(app)
-    world.response = world.app.post('/api/foodcart/restaurants/', data=json.dumps(world.restaurant))
-
-@step(u'Given the restaurant with an id \'([^\']*)\'')
-def given_the_restaurant_with_an_id_group1(step, resto_id):
-    world.resto_id = resto_id
-    world.restaurant = world.app.get('/api/foodcart/restaurants/{}'.format(resto_id))
-    world.response_json = json.loads(world.restaurant.data)
-    
-
-@step(u'When  the view button is clicked')
-def when_the_view_button_is_clicked(step):
-    world.response = world.app.get('/api/foodcart/restaurants/{}'.format(world.resto_id))
-    
-
-@step(u'And   the following details will be returned')
-def and_the_following_details_will_be_returned(step):
-	response_json = json.loads(world.response.data)
-	assert_equals(world.response_json['entries'], response_json['entries'])
-
-
-@step(u'Given the details of restaurant')
-def given_the_details_of_restaurant(step):
-    world.restaurant_oldInfo = step.hashes[0]
-
-
-@step(u'And the new details of retaurant')
-def and_the_new_details_of_retaurant(step):
-    world.restaurant_updatedInfo = step.hashes[0]
-
-
-@step(u'When  the update button is clicked')
-def when_the_update_button_is_clicked(step):
-    world.response = world.app.put('/api/foodcart/restaurants/', data=json.dumps(world.restaurant_updatedInfo))
-    
-
-    
-
 
 """ Common steps for jsonify response """
 
@@ -68,3 +24,63 @@ def and_it_should_get_a_field_group1_containing_group2(step, field, expected_val
     world.response_json = json.loads(world.response.data)
     assert_equals(str(world.response_json[field]), expected_value)
 
+
+@step(u'And   the following details will be returned')
+def and_the_following_details_will_be_returned(step):
+    response_json = json.loads(world.response.data)
+    assert_equals(world.response_json['entries'], response_json['entries'])
+
+
+
+""" Steps for Restaurant feature """
+
+""" Add Restaurant """
+
+@step(u'Given the system administrator have the following restaurant details:')
+def given_the_system_administrator_have_the_following_restaurant_details(step):
+    world.restaurant = step.hashes[0]
+
+@step(u'When  the system administrator clicks the add button')
+def when_the_system_administrator_clicks_the_add_button(step):
+    world.browser = TestApp(app)
+    world.response = world.app.post('/api/foodcart/restaurants/', data=json.dumps(world.restaurant))
+
+
+""" View Restaurant """
+
+@step(u'Given the restaurant with an id \'([^\']*)\'')
+def given_the_restaurant_with_an_id_group1(step, resto_id):
+    world.resto_id = resto_id
+    world.restaurant = world.app.get('/api/foodcart/restaurants/{}'.format(resto_id))
+    world.response_json = json.loads(world.restaurant.data)
+
+@step(u'When  the view button is clicked')
+def when_the_view_button_is_clicked(step):
+    world.response = world.app.get('/api/foodcart/restaurants/{}'.format(world.resto_id))    
+
+
+""" Update Restaurant """
+
+@step(u'And the old details of the restuarant')
+def and_the_old_details_of_the_restuarant(step):
+    world.restaurant_oldInfo = step.hashes[0]
+
+@step(u'And the new details of retaurant')
+def and_the_new_details_of_retaurant(step):
+    world.restaurant_updatedInfo = step.hashes[0]
+
+@step(u'When  the update button is clicked')
+def when_the_update_button_is_clicked(step):
+    world.response = world.app.put('/api/foodcart/restaurants/1', data=json.dumps(world.restaurant_updatedInfo))
+
+
+""" Deactivate Restaurant """
+
+@step(u'Given the restaurant id \'([^\']*)\' is in the database')
+def given_the_restaurant_id_group1_is_in_the_database(step, resto_id):
+    world.resto_id = resto_id
+
+@step(u'When  the deactivate button is clicked')
+def when_the_deactivate_button_is_clicked(step):
+    world.browser = TestApp(app)
+    world.response = world.app.put('/api/foodcart/restaurants/deactivate/{}'.format(world.resto_id))
