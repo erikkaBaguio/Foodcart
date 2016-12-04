788aa69df2a8397d5ae19c5a023f4fd72c6196ce
Kristel Pabillaran <pabillarankristel@ymail.com>
2016-12-04 13:52:31 +0800

    [Update User] - Testing - Added steps in steps.py

--- src/serverside/test/bdd/steps/steps.py
+++ src/serverside/test/bdd/steps/steps.py
@@ -13,44 +13,53 @@ def before_all():
 
 """Add new restaurant"""
 
+
 @step(u'Given the system administrator have the following restaurant details:')
 def given_the_system_administrator_have_the_following_restaurant_details(step):
     world.restaurant = step.hashes[0]
+
 
 @step(u'When  the system administrator clicks the send button')
 def when_the_system_administrator_clicks_the_send_button(step):
     world.browser = TestApp(app)
     world.response = world.app.post('/api/foodcart/restaurants/', data=json.dumps(world.restaurant))
 
+
 @step(u'Given the restaurant with an id \'([^\']*)\'')
 def given_the_restaurant_with_an_id_group1(step, resto_id):
     world.resto_id = resto_id
     world.restaurant = world.app.get('/api/foodcart/restaurants/{}'.format(resto_id))
     world.response_json = json.loads(world.restaurant.data)
-    
+
 
 @step(u'When  the view button is clicked')
 def when_the_view_button_is_clicked(step):
     world.response = world.app.get('/api/foodcart/restaurants/{}'.format(world.resto_id))
-    
+
 
 @step(u'And   the following details will be returned')
 def and_the_following_details_will_be_returned(step):
-	response_json = json.loads(world.response.data)
-	assert_equals(world.response_json['entries'], response_json['entries'])
-    
+    response_json = json.loads(world.response.data)
+    assert_equals(world.response_json['entries'], response_json['entries'])
 
 
 """User"""
+"""Adding User"""
+
 
 @step(u'Given I have the following user details:')
 def user_details(step):
     world.user = step.hashes[0]
 
+
 @step(u'When  the user clicks the send button')
 def when_the_user_clicks_the_send_button(step):
     world.browser = TestApp(app)
-    world.response = world.app.post('/api/foodcart/users/signup/', data = json.dumps(world.user))
+    world.response = world.app.post('/api/foodcart/users/signup/', data=json.dumps(world.user))
+
+
+"""Retrieving User"""
+
 
 @step(u'Given the user with an id \'([^\']*)\'')
 def given_the_user_with_an_id_group1(step, id):
@@ -58,10 +67,28 @@ def given_the_user_with_an_id_group1(step, id):
     world.user = world.app.get('/api/foodcart/users/{}/'.format(id))
     world.response_json = json.loads(world.user.data)
 
+
 @step(u'When  view button is clicked')
 def when_the_view_button_is_clicked(step):
     world.response = world.app.get('/api/foodcart/users/{}/'.format(world.user_id))
 
+
+"""Updating User"""
+
+
+@step(u'Given the details of user')
+def given_the_details_of_user(step):
+    world.user_oldInfo = step.hashes[0]
+
+
+@step(u'And the new details of user')
+def and_the_new_details_of_user(step):
+    world.user_updatedInfo = step.hashes[0]
+
+
+@step(u'When  the update button is clicked')
+def when_the_update_button_is_clicked(step):
+    world.response = world.app.put('/api/foodcart/users/update/', data=json.dumps(world.user_updatedInfo))
 
 
 """ Common steps for jsonify response """
@@ -76,4 +103,3 @@ def then_it_should_get_a_group1_response(step, expected_status_code):
 def and_it_should_get_a_field_group1_containing_group2(step, field, expected_value):
     world.response_json = json.loads(world.response.data)
     assert_equals(str(world.response_json[field]), expected_value)
-
