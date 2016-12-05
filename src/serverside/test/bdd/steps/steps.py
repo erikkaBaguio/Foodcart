5300f0094f9869931f8c4fc17eac629b9f118c45
erikkaBaguio <erikka.baguio@gmail.com>
2016-12-05 23:54:23 +0800

    [View Food] - Testing - Added steps in steps.py

--- src/serverside/test/bdd/steps/steps.py
+++ src/serverside/test/bdd/steps/steps.py
@@ -113,3 +113,16 @@ def given_following_food_details(step):
 def when_add_button_is_clicked(step):
     world.browser = TestApp(app)
     world.response = world.app.post('/api/foodcart/foods/', data=json.dumps(world.restaurant))
+
+
+""" View Food """
+
+@step(u'Given the food with an id \'([^\']*)\'')
+def given_the_food_with_an_id_group1(step, food_id):
+    world.food_id = food_id
+    world.food = world.app.get('/api/foodcart/foods/{}'.format(food_id))
+    world.response_json = json.loads(world.food.data)
+
+@step(u'When  the view button is clicked')
+def when_the_view_button_is_clicked(step):
+    world.response = world.app.get('/api/foodcart/foods/{}'.format(world.food_id)) 
