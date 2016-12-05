9192d51ef77d6de93b0ba828b7bdf974948fe7db
erikkaBaguio <erikka.baguio@gmail.com>
2016-12-05 23:49:21 +0800

    [View Food] - Identify the nouns and operations and implement route for RESTful implementation

--- src/serverside/app/views.py
+++ src/serverside/app/views.py
@@ -189,6 +189,29 @@ def get_foods():
         return jsonify({ "status": "FAILED", "message": "No food found", "entries":[]})
 
 
+@app.route('/api/foodcart/foods/<food_id>', methods = ['GET'])
+def get_food(food_id):
+    food = spcalls.spcall('show_food',(food_id, ))
+    entries = []
+
+    if len(food) == 0:
+        return jsonify({ "status": "FAILED", "message": "No food found", "entries":[]})
+
+    elif 'Error' in str(food[0][0]):
+        return jsonify({ "status": "FAILED", "message": food[0][0]})
+
+    else:
+        f = food[0]
+
+        entries.append({ "food_id" : f[0],
+                         "food_name" : f[1],
+                         "description" : f[2],
+                         "unit_cost" : f[3],
+                         "is_active": f[4] }) 
+
+        return jsonify({ "status": "OK", "message": "OK", "entries": entries, "count": len(entries)})
+
+
 @app.after_request
 def add_cors(resp):
     resp.headers['Access-Control-Allow-Origin'] = flask.request.headers.get('Origin', '*')
