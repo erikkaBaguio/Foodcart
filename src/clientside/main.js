198bb1c4edc499fca43f0f4fc9bee501bedaa174
Kristel Pabillaran <pabillarankristel@ymail.com>
2016-12-06 00:38:26 +0800

    [Search User] - Link the route to the UI using AJAX

--- src/clientside/main.js
+++ src/clientside/main.js
@@ -438,3 +438,62 @@ function updateUser(id){
  	        }
  	    });
 }
+
+
+function searchUser(){
+	var search = $('#user-search').val();
+
+	var data = JSON.stringify({ 'search' : search });
+
+	$.ajax({
+	    	type:"POST",
+	    	url: "http://localhost:5000/api/foodcart/users/search/",
+	    	contentType:"application/json; charset=utf-8",
+			data:data,
+			dataType:"json",
+
+			success: function(results){
+				if (results.status == 'OK'){
+
+					$('#search-user-table-body').html(function(){
+					var user_row = '';
+					var user;
+
+					for (var i = 0; i < results.entries.length; i++) {
+						restaurant = '<tr>' +
+										'<td>' + results.entries[i].fname + results.entries[i].mname + results.entries[i].lname+ '</td>' +
+										'<td>' + results.entries[i].address + '</td>' +
+										'<td>' + results.entries[i].email + '</td>' +
+										'<td>' + results.entries[i].mobile_number + '</td>' +
+										'<td>' + results.entries[i].role_id + '</td>' +
+										'<td>' + results.entries[i].earned_points + '</td>' +
+										'</tr>';
+
+						user_row  += user
+					}
+
+					return user_row;
+				})
+
+				}
+
+				if(results.status == 'FAILED'){
+
+					$('#user-search-alert').html(
+						'<div class="alert alert-danger"><strong>Failed ' +
+						 '!</strong>' + results.message +'</div>');
+
+					$("#user-search-alert").fadeTo(2000, 500).slideUp(500);
+
+				}
+			},
+			error: function(e){
+				alert("THIS IS NOT COOL. SOMETHING WENT WRONG: " + e);
+			},
+			beforeSend: function (xhrObj){
+
+	      		xhrObj.setRequestHeader("Authorization", "Basic " + btoa( auth_user ));
+
+	        }
+	    });
+}
