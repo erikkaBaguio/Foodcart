f4b1a986ee1fd3e42628510fd0f610aa69e0ec38
Kristel Pabillaran <pabillarankristel@ymail.com>
2016-12-06 02:10:01 +0800

    [Deactivate User] - Link route to the UI using AJAX

--- src/clientside/main.js
+++ src/clientside/main.js
@@ -281,6 +281,7 @@ function viewAllUser(){
 										'<td>' + results.entries[i].fname + ' ' + results.entries[i].lname + '</td>' +
 										'<td>'+'<button onclick="viewUserById('+ results.entries[i].id +'); $(\'#view-user\').show();$(\'#view-all-user\').hide()" class="btn btn-info">View</button>'+'</td>'+
 										'<td>' + '<button onclick="updateUser('+ results.entries[i].id +'); $(\'#update-user-form\').show(); $(\'#view-all-user\').hide()" class="btn btn-info">Update</button>'+'</td>'
+										'<td>'+'<button onclick="deactivateUser('+ results.entries[i].id +'); $(\'#view-user\').show()" class="btn btn-danger">Deactivate</button>'+'</td>'+
 									 '</tr>';
 
 						user_row  = user_row + user
@@ -496,4 +497,34 @@ function searchUser(){
 
 	        }
 	    });
+}
+
+
+function deactivateUser(id){
+	$.ajax({
+		type: "PUT",
+		url: "http://localhost:5000/api/foodcart/users/deactivate/" + id + "/",
+		contentType:"application/json; charset=utf-8",
+		dataType:"json",
+
+		success: function(results){
+				if (results.status == 'OK'){
+
+					$('#update-alert').html(
+						'<div class="view-user-alert"><strong>Success ' +
+						 '!</strong>' + results.message +'</div>');
+
+					$("#update-alert").fadeTo(2000, 500).slideUp(500);
+
+				}
+			},
+			error: function(e){
+				alert("THIS IS NOT COOL. SOMETHING WENT WRONG: " + e);
+			},
+			beforeSend: function (xhrObj){
+
+	      		xhrObj.setRequestHeader("Authorization", "Basic " + btoa( auth_user ));
+
+	        }
+	});
 }
