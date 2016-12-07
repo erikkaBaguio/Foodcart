fc56e91714615df5f9b7754c73ada2088ae74bfa
Kristel Pabillaran <pabillarankristel@ymail.com>
2016-12-07 15:17:20 +0800

    Added Address table

--- src/serverside/db_scripts/tables.sql
+++ src/serverside/db_scripts/tables.sql
@@ -5,3 +5,12 @@ create table Contact
   tel_number      VARCHAR(50),
   mobile_number   VARCHAR(50)
 );
+
+
+create table Address
+(
+  address_id      SERIAL8 PRIMARY KEY,
+  bldg_number     VARCHAR(15),
+  street          VARCHAR(50),
+  room_number     INT
+);
