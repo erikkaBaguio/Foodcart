aaee17f6a723449905e295d1ea5b9cb05d6e3f94
Kristel Pabillaran <pabillarankristel@ymail.com>
2016-12-07 15:18:10 +0800

    Modified Users table

--- src/serverside/db_scripts/tables.sql
+++ src/serverside/db_scripts/tables.sql
@@ -21,3 +21,19 @@ create table Image
   image_id        SERIAL8 PRIMARY KEY,
   url             VARCHAR(50)
 );
+
+
+create type Role AS ENUM ('customer', 'system admin', 'manager');
+create table Users
+(
+  user_id            SERIAL8 PRIMARY KEY,
+  fname              VARCHAR(50),
+  mname              VARCHAR(50),
+  lname              VARCHAR(50),
+  user_password      VARCHAR(50),
+  earned_points      FLOAT,
+  contact_id         INT REFERENCES Contact(contact_id),
+  address_id         INT REFERENCES Address(address_id),
+  rolename           Role,
+  is_active          BOOLEAN DEFAULT TRUE
+);
