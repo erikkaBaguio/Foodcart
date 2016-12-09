c91bf3d3cfbba44aa133753a076ff83d1c7f1497
Kristel Pabillaran <pabillarankristel@ymail.com>
2016-12-10 01:39:27 +0800

    [Add User] - Modify functions in stored procedure - Added create_contact_address trigger

--- src/serverside/db_scripts/functions.sql
+++ src/serverside/db_scripts/functions.sql
@@ -1,17 +1,15 @@
---[POST] Add new contact
---select new_contact()
-create or replace function store_contact(par_email VARCHAR, par_telno VARCHAR, par_mobile VARCHAR)
-	returns text as
+--this trigger will create a contact and address where id == to the newly created user
+--but this will not store any other attribute of contact and address. only the id.
+create or replace function create_contact() returns trigger as
 	$$
-		DECLARE
-			loc_res TEXT;
-		BEGIN
-        insert into Contact(email, tel_number, mobile_number)
-        values(par_email, par_telno, par_mobile);
-
-			  loc_res = 'OK';
-
-			RETURN loc_res;
-		END;
+		begin
+			if tg_op = 'INSERT' then
+				insert into Contacts(id)
+					values (new.id);
+				insert into Address(id)
+					values (new.id);
+			end if;
+			return new;
+		end;
 	$$
-		language 'plpgsql';
+		LANGUAGE 'plpgsql';
