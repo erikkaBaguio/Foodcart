6538383f7d2bb0e4e6f6e2858808d6de6c913eff
erikkaBaguio <erikka.baguio@gmail.com>
2016-12-10 00:19:36 +0800

    [Add Restuarant] - Added create_trigger.sql

--- src/serverside/db_scripts/functions.sql
+++ src/serverside/db_scripts/functions.sql
@@ -56,9 +56,9 @@ create or replace function check_restaurant(par_restoName varchar)
 		language 'plpgsql';
 
 
-	--[POST] Adds new restaurant branch
-	--select store_restaurant();
-	create or replace function store_restaurant_branch(par_restoID bigint, par_deliveryFee float, par_restoName varchar)
+--[POST] Adds new restaurant branch
+--select store_restaurant_branch(1, 10.5 );
+create or replace function store_restaurant_branch(par_restoID bigint, par_deliveryFee float)
 	returns bigint as
 	$$
 		declare
@@ -68,10 +68,12 @@ create or replace function check_restaurant(par_restoName varchar)
 
 		begin
 
-			insert into Restaurant_branch(delivery_fee, resto_id)
+			insert into Restaurant_branch(del_fee, resto_id)
 			  values (par_deliveryFee, par_restoID);
 
 			select into local_response currval(pg_get_serial_sequence('Restaurant_branch','id'));
+
+			return local_response;
 
 		end;
 	$$
@@ -142,231 +144,5 @@ create or replace function update_image(par_id int, in par_url varchar)
 
 			return local_response;
 		end;
-	$$
-		language 'plpgsql';
-
---//---------------------------------------
-
-
---[GET] Retrieve specific restaurant
---select show_all_restaurant();
-create or replace function show_all_restaurant(out bigint, out varchar, out float, out float, out varchar, out boolean)
-	returns setof record as
-	$$
-		select *
-		from Restaurant
-	$$
-	language 'sql';
-
-
---[GET] Retrieve specific restaurant
---select show_restaurant(1);
-create or replace function show_restaurant(in par_restoID bigint, out bigint, out varchar, out float, out float, out varchar, out boolean)
-	returns setof record as
-	$$
-		select *
-		from Restaurant
-		where Restaurant.id = par_restoID
-	$$
-	language 'sql';
-
-
---[PUT] Update resaturant
---select update_restaurant('Jollibee', 20, 2, 'Tibanga, Iligan City')
-create or replace function update_restaurant(in par_restoID bigint, in par_restoName varchar, in par_minOrder float, in par_deliveryFee float, in par_location varchar)
-	returns text as
-	$$
-		declare
-			local_response text;
-		begin
-			update Restaurant
-			set resto_name = par_restoName,
-				min_order = par_minOrder,
-				delivery_fee = par_deliveryFee,
-				location = par_location
-			where id = par_restoID;
-			
-			local_response = 'OK';			
-			return local_response;
-		end;
-	$$
-	language 'plpgsql';
-
-
---[DELETE] Deactivates restaurant
---select delete_restaurant(1);
-create or replace function delete_restaurant(in par_restoID bigint) 
-	returns text as
-	$$
-		declare
-			local_response text;
-		begin
-			update Restaurant
-			set is_active = False
-			where id = par_restoID;
-
-			local_response = 'SUCCESS';
-			return local_response;
-		end;
-	$$
-	language 'plpgsql';
-
-
---Returns set of restaurants that match or slightly match your search
---select search_restaurant('Iligan');
---source: http://www.tutorialspoint.com/postgresql/postgresql_like_clause.htm
---source on concationation in postgres: http://www.postgresql.org/docs/9.1/static/functions-string.html
-create or replace function search_restaurant(in par_search text, out bigint, out varchar, out float, out float, out varchar, out boolean) returns setof record as
-  $$
-      select * 
-      from Restaurant
-      where resto_name like '%'|| par_search || '%' or location like '%'|| par_search || '%';
-  $$
-  language 'sql';
-
-
-
-
-------------
---  FOOD  --
-------------
-
---[POST] Add food
---select store_food('test food', 'test description', 1);
-create or replace function store_food(par_food_name varchar, par_description text, par_unit_cost float)
-	returns text as
-	$$
-		declare
-			local_food_name	varchar;
-			local_response text;
-
-		begin
-			select into local_food_name food_name
-			from Food
-			where food_name = par_food_name;
-
-			if local_food_name isnull
-			then
-				insert into Food(food_name, description, unit_cost)
-					values (par_food_name, par_description, par_unit_cost);
-
-				local_response = 'OK';
-			else
-				local_response = 'EXISTED';
-			
-			end if;
-			return local_response;
-
-		end;
-	$$
-	language 'plpgsql';
-
-
---[GET] View all food
---select show_all_food();
-create or replace function show_all_food(out bigint, out varchar, out text, out float, out boolean)
-	returns setof record as
-	$$
-		select *
-		from Food;
-	$$
-	language 'sql';
-
-
---[GET] View food
---select show_food(1);
-create or replace function show_food(in par_foodID bigint, out bigint, out varchar, out text, out float, out boolean)
-	returns setof record as
-	$$
-		select *
-		from Food
-		where id = par_foodID;
-	$$
-	language 'sql';
-
-
---[PUT] Update food
---select update_food(1,'Madcow', 'The original hot and spicy pizza in the world.',299);
-create or replace function update_food(in par_foodID bigint, in par_food_name varchar, in par_description text, in par_unit_cost float)
-	returns text as
-	$$
-		declare
-			local_response text;
-
-		begin
-			update Food
-				set food_name = par_food_name,
-					description = par_description,
-					unit_cost =  par_unit_cost
-			where id = par_foodID;
-
-			local_response = 'OK';
-			return local_response;
-		end;
-	$$
-	language 'plpgsql';
-
-
---[DELETE] Deactivate food
---select delete_food(1);
-create or replace function delete_food(in par_foodID bigint)
-	returns text as 
-	$$
-		declare
-			local_response text;
-
-		begin
-			update Food
-				set is_active = False
-			where id = par_foodID;
-
-			local_response = 'SUCCESS';
-			return local_response;
-		end;
-	$$
-	language 'plpgsql';
-
-
---Returns set of foods that match or slightly match your search
---select search_food('');
---source: http://www.tutorialspoint.com/postgresql/postgresql_like_clause.htm
---source on concationation in postgres: http://www.postgresql.org/docs/9.1/static/functions-string.html
-create or replace function search_food(in par_search varchar, out bigint, out varchar, out text, out float, out boolean)
-	returns setof record as
-	$$
-		select *
-		from Food
-		where food_name like '%' || par_search || '%' or description like '%' || par_search || '%' ;
-	$$
-	language 'sql';
-
-
---[POST] Add new category details
---select store_category('Appetizer')
-create or replace function store_category(par_categoryName varchar)
-	returns text as
-	$$
-		DECLARE
-			local_name	varchar;
-			local_response text;
-		BEGIN
-
-			select into local_name category_name
-			from Categories
-			where category_name = par_categoryName;
-
-			if local_name isnull
-			then
-				insert into Categories(category_name, is_active)
-				values (par_categoryName, TRUE);
-
-				local_response = 'OK';
-			else
-				local_response = 'EXISTED';
-
-			end if;
-
-			return local_response;
-		END;
 	$$
 		language 'plpgsql';
---
+++ src/serverside/db_scripts/triggers.sql
@@ -0,0 +1,18 @@
+--this trigger will create a contact and addresses where id == to the newly created restaurant branch.
+--but this will not store any other attribute of contacts and addresses. only the id.
+create or replace function create_restaurant_branch() returns trigger as
+	$$
+		begin
+			if tg_op = 'INSERT' then
+				insert into Contacts(id)
+					values (new.id);
+				insert into Address(id)
+					values (new.id);
+			end if;
+			return new;
+		end;
+	$$
+		LANGUAGE 'plpgsql';
+
+create trigger create_restaurant_branch_ins AFTER insert on Restaurant_branch FOR each ROW
+EXECUTE PROCEDURE create_restaurant_branch();
