a3e4c493f1a7087c5f70e65f402c9359f17d54d0
erikkaBaguio <erikka.baguio@gmail.com>
2016-12-10 08:49:36 +0800

    [Add Restaurant] - Added trigger function for creating restaurant

--- src/serverside/db_scripts/triggers.sql
+++ src/serverside/db_scripts/triggers.sql
@@ -16,3 +16,21 @@ create or replace function create_restaurant_branch() returns trigger as
 
 create trigger create_restaurant_branch_ins AFTER insert on Restaurant_branch FOR each ROW
 EXECUTE PROCEDURE create_restaurant_branch();
+
+
+--this trigger will create an image where id == to the newly created restaurant.
+--but this will not store any other attribute of image, only the id.
+create or replace function create_restaurant() returns trigger as
+	$$
+		begin
+			if tg_op = 'INSERT' then
+				insert into Images(id)
+					values (new.id);
+			end if;
+			return new;
+		end;
+	$$
+		LANGUAGE 'plpgsql';
+
+create trigger create_restaurant_ins AFTER insert on Restaurants FOR each ROW
+EXECUTE PROCEDURE create_restaurant();
