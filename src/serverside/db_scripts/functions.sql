4515880322b06a362f801241eb9faeb5729c7e0d
Kristel Pabillaran <pabillarankristel@ymail.com>
2016-12-12 11:20:31 +0800

    [Add User] - Modify store_user function - check if user exists or not

--- src/serverside/db_scripts/functions.sql
+++ src/serverside/db_scripts/functions.sql
@@ -1,14 +1,17 @@
 --select store_user('Ma.', 'Erikka', 'Baguio', 'asdasd', 1);
 create or replace function store_user(in par_fname varchar, in par_mname varchar, in par_lname varchar, in par_password varchar,
-									  in par_rolename int)
+                    in par_rolename int)
   returns bigint as
   $$
     declare
+      local_name text;
       local_response bigint;
     begin
 
-      insert into Users (fname, mname, lname, user_password, role_id)
-      values (par_fname, par_mname, par_lname, par_password, par_rolename);
+      select into local_name fname || lname from Users where fname = par_fname and lname = par_lname;
+      IF local_name isnull then
+        insert into Users (fname, mname, lname, user_password, role_id)
+        values (par_fname, par_mname, par_lname, par_password, par_rolename);
 
       SELECT INTO local_response currval(pg_get_serial_sequence('Users','id'));
 
@@ -18,6 +21,13 @@ create or replace function store_user(in par_fname varchar, in par_mname varchar
       where id = local_response;
 
       return local_response;
+
+      ELSE
+        --local_response returns 0 if user exists
+        local_response = 0;
+
+      END IF;
+      RETURN local_response;
 
     end;
   $$
