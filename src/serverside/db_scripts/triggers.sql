4a39372b8c0e133292dc31c9c2c0d12f3d9ed6ac
erikkaBaguio <erikka.baguio@gmail.com>
2016-12-12 10:12:20 +0800

    [Add Restaurant] - Modify all functions in trigges.sql

--- src/serverside/db_scripts/triggers.sql
+++ src/serverside/db_scripts/triggers.sql
@@ -1,12 +1,12 @@
 --this trigger will create a contact and addresses where id == to the newly created restaurant branch.
---but this will not store any other attribute of contacts and addresses. only the id.
+--but this will not store any other attribute of Resto_branch_contacts and Resto_branch_address. only the id.
 create or replace function create_restaurant_branch() returns trigger as
 	$$
 		begin
 			if tg_op = 'INSERT' then
-				insert into Contacts(id)
+				insert into Resto_branch_contacts(id)
 					values (new.id);
-				insert into Address(id)
+				insert into Resto_branch_address(id)
 					values (new.id);
 			end if;
 			return new;
@@ -18,13 +18,13 @@ create trigger create_restaurant_branch_ins AFTER insert on Restaurant_branch FO
 EXECUTE PROCEDURE create_restaurant_branch();
 
 
---this trigger will create an image where id == to the newly created restaurant.
---but this will not store any other attribute of image, only the id.
+--this trigger will create an Resto_image where id == to the newly created restaurant.
+--but this will not store any other attribute of Resto_image, only the id.
 create or replace function create_restaurant() returns trigger as
 	$$
 		begin
 			if tg_op = 'INSERT' then
-				insert into Images(id)
+				insert into Resto_images(id)
 					values (new.id);
 			end if;
 			return new;
