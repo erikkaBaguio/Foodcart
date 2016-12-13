9d972af4e8f1f6f0642fa4bac6d442a209f1851d
erikkaBaguio <erikka.baguio@gmail.com>
2016-12-13 23:19:29 +0800

    [Add Food] - Modify functions in stored procedure - Added trigger function

--- src/serverside/db_scripts/triggers.sql
+++ src/serverside/db_scripts/triggers.sql
@@ -34,3 +34,21 @@ create or replace function create_restaurant() returns trigger as
 
 create trigger create_restaurant_ins AFTER insert on Restaurants FOR each ROW
 EXECUTE PROCEDURE create_restaurant();
+
+
+--this trigger will create an Food_image where id == to the newly created food.
+--but this will not store any other attribute of Food_image, only the id.
+create or replace function create_food() returns trigger as
+	$$
+		begin
+			if tg_op = 'INSERT' then
+				insert into Food_images(id)
+					values (new.id);
+			end if;
+			return new;
+		end;
+	$$
+		LANGUAGE 'plpgsql';
+
+create trigger create_food_ins AFTER insert on Foods FOR each ROW
+EXECUTE PROCEDURE create_food();
