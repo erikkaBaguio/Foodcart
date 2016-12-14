455ba73ea38fe6b6a3cdb22c83967eb0178cec19
Kristel Pabillaran <pabillarankristel@ymail.com>
2016-12-15 01:41:00 +0800

    [Users] - Add triggers for user

--- src/serverside/db_scripts/triggers.sql
+++ src/serverside/db_scripts/triggers.sql
@@ -52,3 +52,28 @@ create or replace function create_food() returns trigger as
 
 create trigger create_food_ins AFTER insert on Foods FOR each ROW
 EXECUTE PROCEDURE create_food();
+
+
+------------
+--  USER  --
+------------
+
+--this trigger will create a contact and address where id == to the newly created user
+--but this will not store any other attribute of contact and address. only the id.
+create or replace function create_contact_address() returns trigger as
+	$$
+		begin
+			if tg_op = 'INSERT' then
+				insert into User_contacts(id)
+					values (new.id);
+				insert into User_address(id)
+					values (new.id);
+			end if;
+			return new;
+		end;
+	$$
+		LANGUAGE 'plpgsql';
+
+
+create trigger create_user_trigger AFTER insert on Users FOR each ROW
+EXECUTE PROCEDURE create_contact_address();
