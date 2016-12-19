653528ac360ed8f2a081d8138f75fa4e854ca8c6
Kristel Pabillaran <pabillarankristel@ymail.com>
2016-12-19 12:29:55 +0800

    [Add User] - Modify store_user function

--- src/serverside/app/users.py
+++ src/serverside/app/users.py
@@ -12,16 +12,92 @@ spcalls = SPcalls()
 
 
 
+# def store_user(data):
+#     user_email = data['user_email']
+#     tel_num = data['user_tel_number']
+#     mobile_num = data['user_mobile_number']
+#     bldg_num = data['user_bldg_number']
+#     street = data['user_street']
+#     room_num = data['user_room_number']
+#
+#
+#     check_email_exist = spcalls.spcall('check_email', (user_email,))
+#
+#     if check_email_exist[0][0] == 'OK':
+#
+#         check_email = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', user_email)
+#
+#         if check_email is not None:
+#             fname = data['fname']
+#             mname = data['mname']
+#             lname = data['lname']
+#             password = data['user_password']
+#             role_id = data['role_id']
+#
+#             complete_fields = fname == '' or mname == '' or lname == '' or password == '' or not role_id or user_email == '' or tel_num == '' or mobile_num == '' or bldg_num == '' or street == '' or room_num == ''
+#
+#             if complete_fields is True:
+#                 """
+#                 PASSWORD HASHING
+#                 source: https://pythonprogramming.net/password-hashing-flask-tutorial/
+#
+#                 import hashlib
+#                 password = 'pa$$w0rd'
+#                 h = hashlib.md5(password.encode())
+#                 print(h.hexdigest())
+#
+#                 """
+#                 pw_hash = hashlib.md5(password.encode())
+#
+#                 user = spcalls.spcall('store_user', (fname, mname, lname, pw_hash.hexdigest(), role_id), True)
+#
+#                 if 'Error' in str(user[0][0]):
+#                     return jsonify({"status": "error", "message": user[0][0]})
+#
+#                 else:
+#                     user_id = user[0][0]
+#
+#                     contact = spcalls.spcall('update_user_contact', (
+#                     user_id, data['user_email'], data['user_tel_number'], data['user_mobile_number']), True)
+#                     address = spcalls.spcall('update_user_address', (
+#                     user_id, data['user_bldg_number'], data['user_street'], data['user_room_number']), True)
+#
+#                     if 'Error' in str(contact[0][0]):
+#                         return jsonify({"status": "FAILED", "message": contact[0][0]})
+#
+#                     elif 'Error' in str(address[0][0]):
+#                         return jsonify({"status": "FAILED", "message": address[0][0]})
+#
+#                     else:
+#                         return jsonify({"status": "OK", "message": address[0][0]})
+#
+#                 # return jsonify({"status": "OK", "message": user[0][0]})
+#
+#             elif complete_fields is False:
+#                 return jsonify({"status": 'FAILED', 'message': 'Please input required fields!'})
+#
+#         else:
+#             return jsonify({'status': 'FAILED', 'message': 'Invalid email input!'})
+#
+#     elif check_email_exist[0][0] == 'EXISTED':
+#         return jsonify({'status': 'FAILED', 'message': 'Email already exists'})
+#
+#     else:
+#         return jsonify({'status': 'FAILED'})
+
+
+
 def store_user(data):
-    if (data['fname'] == '' or data['mname'] == '' or data['lname'] == '' or data['user_password'] == '' or not data['role_id'] or
-        data['user_email'] == '' or data['user_tel_number'] == '' or data['user_mobile_number'] == ''
-        or data['user_bldg_number'] == '' or data['user_street'] == '' or data['user_room_number'] == ''):
+    password = data['user_password']
+    email = data['user_email']
 
-        return jsonify({"status": "FAILED", "message": "Please fill the required fields"})
+    check_email_exist = spcalls.spcall('check_email', (email,))
 
-    else:
+    if check_email_exist[0][0] == 'OK':
+        pw_hash = hashlib.md5(password.encode())
+
         user = spcalls.spcall('store_user', (
-            data['fname'], data['mname'], data['lname'], data['user_password'], data['role_id']), True)
+            data['fname'], data['mname'], data['lname'], pw_hash.hexdigest(), data['role_id']), True)
 
         if 'Error' in str(user[0][0]):
             return jsonify({"status": "error", "message": user[0][0]})
@@ -42,6 +118,12 @@ def store_user(data):
                 return jsonify({"status": "OK", "message": address[0][0]})
 
         return jsonify({"status": "OK", "message": user[0][0]})
+
+    elif (data['fname'] == '' or data['mname'] == '' or data['lname'] == '' or data['user_password'] == '' or not data['role_id'] or
+        data['user_email'] == '' or data['user_tel_number'] == '' or data['user_mobile_number'] == ''
+        or data['user_bldg_number'] == '' or data['user_street'] == '' or data['user_room_number'] == ''):
+
+        return jsonify({"status": "FAILED", "message": "Please fill the required fields"})
 
 
 def get_user_id(id):
