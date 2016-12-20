471baaa49d83b6b5becd2a4e7a1f4a7c0347127e
erikkaBaguio <erikka.baguio@gmail.com>
2016-12-20 10:50:45 +0800

    Modified init.pp

---
+++ puppet/manifests/init.pp
@@ -0,0 +1,16 @@
+# Python Dev
+
+	class pythondev{
+		require yum
+		include iptables
+		include misc
+		include httpd
+		include download
+		include python
+		include pythondevel
+		include pythonextras
+	}
+
+	include pythondev
+
+
